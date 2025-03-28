080f48d6fc5a39396642335ed04ac21a24cc07a9
Javi Broz <javibroz@gmail.com>
2025-03-28 21:01:48 +0100

    Finalizado. Solo falta que el borde del tablero se ajuste al contenido

--- dibujar.js
+++ dibujar.js
@@ -8,6 +8,7 @@ function dibujarTablero(filas, columnas) {
             celda.style.border = '1px solid black';
             celda.style.width = '10px';
             celda.style.height = '10px';
+            celda.className = 'celda';
             // celda.style.margin = '2px';
             tablero.appendChild(celda);
 
@@ -17,15 +18,15 @@ function dibujarTablero(filas, columnas) {
 
 
 
-    tablero.style.boxSizing = 'content-box';
-    tablero.style.border = '1px solid black';
+    // tablero.style.boxSizing = 'content-box';
+    tablero.style.border = '3px solid black';
     tablero.style.padding = '2px';
     tablero.style.display = 'grid';
     // tablero.style.gridTemplateColumns = 'repeat(30,0px)';
     // tablero.style.gridTemplateRows = 'repeat(30,0px)';
     // tablero.style.gap = '14px';
-    tablero.style.gridTemplateColumns = 'repeat(30,14px)';
-    tablero.style.gridTemplateRows = 'repeat(30,14px)';
+    tablero.style.gridTemplateColumns = `repeat(${columnas},14px)`;
+    tablero.style.gridTemplateRows = `repeat(${filas},14px)`;
 }
 
 function seleccionarColor() {
@@ -45,19 +46,60 @@ function seleccionarColor() {
             estadoDelPincel.style.backgroundColor = color;
 
 
-            if (!estadoDelPincel) {
-                console.error("Elemento #estadoDelPincel no encontrado en el DOM");
-                return;
-
-            }
-
-
         })
     })
     // console.log(casillaColor.classList.value)
 }
 
+// function pintar() {
+//     const celda = document.querySelector('.celda');
+//     const estadoDelPincel = document.querySelector('#pincel')
+//     const color = document.getComputedStyle(estadoDelPincel).backgroundColor;
+//     celda.style.backgroundColor = color;
+    
+
+// }
 
 
-dibujarTablero(30, 30);
-seleccionarColor();
+function main() {
+    // const tablero = document.getElementById('zonadibujo');
+    
+    dibujarTablero(30,30)
+    seleccionarColor();
+
+    let pincelActivado = false;
+    const cuadrícula = document.querySelectorAll('.celda')
+
+
+    cuadrícula.forEach(celda => {
+        celda.addEventListener('click', function () {
+            const estadoDelPincel = document.querySelector('#pincel');
+            const color = getComputedStyle(estadoDelPincel).backgroundColor;
+            pincelActivado = !pincelActivado;
+            pincelActivado?
+             estadoDelPincel.innerHTML='PINCEL ACTIVADO'
+             : estadoDelPincel.innerHTML='PINCEL DESACTIVADO'
+                     
+            celda.style.backgroundColor = color;
+        })
+      
+    })
+
+
+     cuadrícula.forEach(celda => {
+                celda.addEventListener('mouseover', () => {
+
+                    const estadoDelPincel = document.querySelector('#pincel')
+                    const color = getComputedStyle(estadoDelPincel).backgroundColor;
+                    if (pincelActivado) {celda.style.backgroundColor = color};
+                    
+                })
+            })
+
+      
+}
+
+
+
+
+main();
